993c1a509de598dbc015125b9e70a61e3d8a569d
cdecesaris <sharpsabresword@g.ucla.edu>
2019-04-23 13:56:16 -0700

    updated to create directories

--- Split_GTF.sh
+++ Split_GTF.sh
@@ -3,11 +3,14 @@
 
 #code takes an input file containing three chromosomes and splits
 #the file into three new files for each chromosomes
+mkdir dir_Chr21
+mkdir dir_Chr2
+mkdir dir_Chr3
 
 
-grep "chr21" $1 > chr21.gtf
-grep -P "chr2\t" $1 > chr2.gtf
-grep "chr3" $1 > chr3.gtf
+grep -P "chr21" $1 > dir_Chr21/chr21.gtf
+grep -P "chr2\t" $1 > dir_Chr2/chr2.gtf
+grep -P "chr3" $1 > dir_Chr3/chr3.gtf
 
 
 
